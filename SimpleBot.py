861f29dfccf2cf5520f07946a0764814028849b7
Vipuld14 <vipulsvdhotre@icloud.com>
2026-01-26 11:32:40 -0500

    New URL's Added, and document splitting was setup

--- SimpleBot.py
+++ SimpleBot.py
@@ -11,3 +11,26 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import SKLearnVectorStore
 
+# Docuent Loading
+urls = [
+    "https://catalogs.gsu.edu/preview_program.php?catoid=4&poid=1159",
+    "https://catalogs.gsu.edu/preview_program.php?catoid=43&poid=12713",
+    "https://catalogs.gsu.edu/preview_entity.php?catoid=43&ent_oid=2982",
+    "https://www.gsu.edu/program/computer-science-bs/?utm_source=pltitle&utm_medium=cas&utm_content=bs&utm_campaign=program_explorer",
+    "https://www.gsu.edu/program/computer-science-ms/?utm_source=pltitle&utm_medium=cas&utm_content=ms&utm_campaign=program_explorer",
+    "https://catalogs.gsu.edu/content.php?catoid=42&navoid=5496",
+    "https://catalogs.gsu.edu/content.php?catoid=42&navoid=5496#3010-general-information",
+
+]
+
+documents = [WebBaseLoader(url).load() for url in urls]
+documentList = [doc for subset in documents for doc in subset]
+
+#Document Split/Chunking
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=400,
+    chunk_overlap=80
+)
+
+documentSlit = text_splitter.split_documents(documentList)
+
